728fd7fa9794865bdd9ad5354a74bec0001ae7c2
Badjarda <mestredua@gmail.com>
2023-02-28 15:07:55 -0500

    start project

---
+++ tsp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "nqueue/nqueue/queue.h"
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("Retrieved line of length %zu:\n", read);
+        printf("%s", line);
+    }
+
+    fclose(fp);
+    if (line)
+        free(line);
+    exit(EXIT_SUCCESS);
+}
