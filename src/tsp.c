56d205d0702742fe41f01fd6ebe31f08bd1b28c8
carloscosta2000 <carlosdcosta30@gmail.com>
2023-04-10 13:04:29 +0100

    cahnged name of func

--- src/tsp.c
+++ src/tsp.c
@@ -42,8 +42,8 @@ int main(int argc, char *argv[]) {
 
     double** distances = (double **) malloc(sizeof(double) *n);
     #pragma omp parallel for
-        for(int i = 0; i < n; i++)
-            distances[i] = (double *)malloc(n * sizeof(double));
+    for(int i = 0; i < n; i++)
+        distances[i] = (double *)malloc(n * sizeof(double));
     if(n_edges)
         free(n_edges);
 
