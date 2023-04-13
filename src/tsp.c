a26fb9921db7a1e4f3035204247c11e0cbd35155
carloscosta2000 <carlosdcosta30@gmail.com>
2023-04-13 18:59:22 +0100

    50000 n

--- src/tsp.c
+++ src/tsp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
 
     int id, p;
 
-    MPI_Init (&argc, &argv);
+    MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
