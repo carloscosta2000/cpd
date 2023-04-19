37f18382db472d9b29f3328b7e867d35b69cfb28
carloscosta2000 <carlosdcosta30@gmail.com>
2023-04-19 14:37:23 +0100

    Test MPI exam

---
+++ test_mpi.c
@@ -0,0 +1,60 @@
+#include <omp.h>
+#include <mpi.h>
+#define TAG 123
+#define TAG_BTC 111
+#define N 6
+int main(int argc, char *argv[]) {
+
+    int rank, p;
+    int[N] a;
+    int[N] b;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    if (rank == 0)
+         for (i = 0; i < N; i++)
+            a[i] = i;
+
+    printf("Before line 5\n");
+    printf("A\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", a[i]);
+    }
+    printf("b\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", b[i]);
+    }
+    MPI_Bcast(a, N, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("Before line 6\n");
+    printf("A\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", a[i]);
+    }
+    printf("b\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", b[i]);
+    }
+    for (i = 0; i < N; i++)
+        b[i] = a[i] + rank;
+
+    printf("Before line 8\n");
+    printf("A\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", a[i]);
+    }
+    printf("b\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", b[i]);
+    }
+    MPI_Alltoall(b, N, MPI_INT, a, N, MPI_INT, MPI_COMM_WORLD);
+    printf("After line 8\n");
+    printf("A\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", a[i]);
+    }
+    printf("b\n");
+    for (int asd = 0; asd < N; asd++) {
+        printf("%d", b[i]);
+    }
+}
