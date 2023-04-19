9e1f64d86b56f8e6f564f1c4bcd0025577d11843
carloscosta2000 <carlosdcosta30@gmail.com>
2023-04-19 16:14:14 +0100

    Test MPI exam

--- test_mpi.c
+++ test_mpi.c
@@ -65,26 +65,20 @@ int main(int argc, char *argv[]) {
     sleep(5);
     if (rank == 0) {
         printf("After line 8\n");
-        printf("A\n");
         for (int asd = 0; asd < N; asd++) {
-            printf("%d_ ", a[asd]);
+            printf("a: %d_ ", a[asd]);
         }
-        printf("\n");
-        printf("b\n");
         for (int asd = 0; asd < N; asd++) {
-            printf("%d_ ", b[asd]);
+            printf("b: %d_ ", b[asd]);
         }
 
     } else {
         printf("After line 8\n");
-        printf("A\n");
         for (int asd = 0; asd < N; asd++) {
-            printf("%d* ", a[asd]);
+            printf("a: %d* ", a[asd]);
         }
-        printf("\n");
-        printf("b\n");
         for (int asd = 0; asd < N; asd++) {
-            printf("%d* ", b[asd]);
+            printf("b: %d* ", b[asd]);
         }
     }
     printf("\n");
